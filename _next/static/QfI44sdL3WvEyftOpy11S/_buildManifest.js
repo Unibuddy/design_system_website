c96b1f5a6ebcc4ba057c141f560f4945be3d6d7e
ci-build <ci-build@unibuddy.com>
2020-05-14 13:46:12 +0000

    [skip ci] Updates

--- _next/static/QfI44sdL3WvEyftOpy11S/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/components/forms/forms":[a],"/components/icons":["static\u002Fchunks\u002Fc8b05f54.c6e6165f3983aa2a4051.js",a]}}("static\u002Fchunks\u002Fdd5f69787f0d9b7ffb5545ed84b7e1ac7e535039.fc299d28087bbe612f9c.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
